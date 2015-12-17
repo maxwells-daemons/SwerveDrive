ae4b590c301cd942d2a85fea4d0a19ebe1232333
Aidan Swope <Aidan Swope>
2015-12-16 16:44:53 -1000

    Refactored offset name and added argument checking

--- src/Hardware/DigiPot.java
+++ src/Hardware/DigiPot.java
@@ -14,25 +14,38 @@ import edu.wpi.first.wpilibj.AnalogPotentiometer;
 public class DigiPot extends AnalogPotentiometer implements AngularSensor {
     
     // Hardware-dependent //
-    private double _minVoltage;
-    private double _maxVoltage;
-    private double _minVoltageAngle;
+    private final double _minVoltage;
+    private final double _maxVoltage;
+    private final double _offsetDegrees;
     
     
-    public DigiPot(int channel, double minVoltage, double maxVoltage, double minVoltageAngle) { //TODO: Implement direction
+    public DigiPot(int channel, double minVoltage, double maxVoltage, double offsetDegrees) { //TODO: Implement direction
         super(channel);
+        if (minVoltage >= maxVoltage) throw new IllegalArgumentException("Minimum voltage must be less than maximum voltage");
+        if (offsetDegrees < 0 || offsetDegrees > 360) throw new IllegalArgumentException("Offset must be between 0 and 360 degrees");
         
         _minVoltage = minVoltage;
         _maxVoltage = maxVoltage;
-        _minVoltageAngle = minVoltageAngle;
-        
+        _offsetDegrees = offsetDegrees;
     }
     
     public double getDegrees() { //Assume direct linear conversion between minVoltage and maxVoltage
         double voltage = this.get();
         
         //Currently assumes more voltage = more degrees, cannot reverse
-        double degrees = (((voltage - _minVoltage) * (360.0 / _maxVoltage)) + _minVoltageAngle) % 360.0;
+        double degrees = (((voltage - _minVoltage) * (360.0 / _maxVoltage)) + _offsetDegrees) % 360.0;
         return degrees;
+    }
+    
+    public double getMinVoltage() {
+        return _minVoltage;
+    }
+    
+    public double getMaxVoltage() {
+        return _maxVoltage;
+    }
+    
+    public double getOffsetDegrees() {
+        return _offsetDegrees;
     }
 }
