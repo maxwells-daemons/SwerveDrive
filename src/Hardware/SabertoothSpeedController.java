8fcc6c770b99ea2c40434406cf7028bf4bd8f454
Aidan Swope <Aidan Swope>
2015-12-16 16:55:31 -1000

    Implemented Sabertooth class

--- src/Hardware/SabertoothSpeedController.java
+++ src/Hardware/SabertoothSpeedController.java
@@ -5,10 +5,94 @@
  */
 package Hardware;
 
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.visa.VisaException;
+
 /**
  *
  * @author Aidan
  */
-public class SabertoothSpeedController {
+public class SabertoothSpeedController implements SpeedController {
     
+    private static SerialPort _PORT; //Serial port shared by all Sabertooths
+    
+    //TODO: Keep list of assigned motors (address/motor pairs) and throw an error when we overwrite one?
+    
+    // Motor specification //
+    private int _address;
+    private int _motorNumber;
+    
+    private double _currentSpeed;
+    
+    public SabertoothSpeedController(int address, int motorNumber) {
+        if (_PORT == null) throw new IllegalStateException("Serial port not initialized, call initializeSerialPort(baudRate)");
+        if (address < 128 || address > 135) throw new IllegalArgumentException("Sabertooth address out of range");
+        if (motorNumber < 1 || motorNumber > 2) throw new IllegalArgumentException("Sabertooth motor number out of range");
+        
+        _address = address;
+        _motorNumber = motorNumber;
+    
+        _currentSpeed = 0.0;
+    }
+    
+    public static void initializeSerialPort(int baudRate) {
+        if(_PORT != null) throw new IllegalStateException("Serial port already initialized");
+        
+        try {
+            _PORT = new SerialPort(baudRate);
+            
+            // send the baud rate character //
+            byte[] baudChar = { (byte) 170 };
+            _PORT.write(baudChar, baudChar.length);
+        } catch(VisaException e) {
+            throw new RuntimeException("Error creating serial port: " + e.getMessage());
+        }
+    }
+    
+    private void writeSerialPacket(int command, int data) throws VisaException {
+        byte addr = (byte) _address;
+        byte com = (byte) command;
+        byte dat = (byte) data;
+        byte checksum = (byte) ((addr + com + dat) & 0x7F);
+        byte[] packet = { addr, com, dat, checksum };
+        _PORT.write(packet, packet.length);
+    }
+    
+    public void pidWrite(double output) {
+        set(output);
+    }   
+
+    public double get() {
+        return _currentSpeed;
+    }
+
+    public void set(double speed, byte syncGroup) {
+        set(speed); //Not sure if I'll ever need to implement sync groups for this
+    }
+
+    public void set(double speed) {
+        double spd = Math.abs(speed);
+        
+        if (spd > 1.0) throw new IllegalArgumentException("Speed must be between -1.0 and 1.0");
+        
+        // Set command byte... TODO: Make this more readable? //
+        int command = (_motorNumber == 1) ? 0 : 4; //Command is 0/1 for motor 1, 4/5 for motor 2
+        if (speed < 0.0) command++;
+        
+        // Set data byte //
+        int data = (int) Math.floor(spd * 128.0);
+        
+        // Write data //
+        try {
+            writeSerialPacket(command, data);
+        } catch (VisaException e) {
+            throw new RuntimeException("Error writing serial data: " + e.getMessage());
+        }
+    }
+
+    public void disable() {
+        set(0.0);
+    }
 }
